af049724456897b14bd74c23614fe9c3438a94da
Samantha Dorrell <samantha.dorrell@yahoo.co.uk>
2023-01-09 10:41:28 +0000

    Update Contact.jsx
    Form works via Netlify

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,67 +1,42 @@
 import "./styles/Contact.css";
-// import {useState} from 'react';
 import { Container } from "react-bootstrap";
 
-// const encode = (data) => {
-//   return Object.keys(data)
-//     .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])).join('&');
-// }
-
 export default function Form (){
-  // const [state, setState] = useState({name: '', email: '', message: '' })
-
-  // const handleChange = e =>
-  //   setState({...state, [e.target.name]: e.target.value })
-
-  // const handleSubmit = e => {
-  //   fetch('/', {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //     body: encode({ 'form-name': 'contactForm', ...state })
-  //   })
-  //     .then(() => alert('Thank you for getting in touch! The message was sent successfully!'))
-  //     .catch(error => alert(error))
-  //   e.preventDefault()
-  // }
-
+  
   return (
     <Container>
     <h1><span className="get"> Get</span> in <span className="contact">Contact</span></h1>
     <form 
       method='POST' 
       className='contact-form' 
-      // onSubmit={handleSubmit}
       data-netlify-recaptcha="true"
       data-netlify="true">
 
       <input type="hidden" name="form-name" value="contactForm" />
 
       <input 
+        aria-label="Enter your name"
         type='text' 
-        name='name' 
-        // value={state.name}
-        placeholder='Enter your name'
-        // onChange={handleChange} 
+        name='name'         
+        placeholder='Your name'         
         />
 
       <input 
+        aria-label="Enter your email"
         type='email' 
-        name='email' 
-        // value={state.email}
-        placeholder='Enter your email'
-        // onChange={handleChange} 
+        name='email'         
+        placeholder='Your email'        
         />
 
       <textarea 
+        aria-label="Enter your message"
         name='message' 
-        placeholder='Messaage'
-        // value={state.message}
-        // onChange={handleChange}
+        placeholder='Your Message'        
         ></textarea>
 
       <div data-netlify-recaptcha="true"></div>
 
-      <button type='submit'>Submit</button>
+      <button aria-label="Send Message Button" type='submit'>Send Message</button>
     </form>
     </Container>
   )
